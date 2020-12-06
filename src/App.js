985c4c8d850533f4d925b3a47f2643372e520234
Matej <matej.navara@gmail.com>
2020-12-06 08:18:49 +0000

    Connected to Firestore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Header from './components/header/header';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
@@ -20,9 +20,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log("🚀 ~ file: App.js ~ line 22 ~ App ~ componentDidMount ~ user", user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+      const userRef = await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot(snapshot => {
+        this.setState({
+          currentUser: {
+            id: snapshot.id,
+            ...snapshot.date()
+          }
+        })
+      })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,29 @@ const config = {
   appId: "1:133612210658:web:2323b2d20db7c842d451bb"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists) {
+    const { displayName, email } = userAuth;
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt: new Date(),
+        ...additionalData,
+      })
+    } catch (error) {
+      console.log("Error creating user", error)
+    }
+  }
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
