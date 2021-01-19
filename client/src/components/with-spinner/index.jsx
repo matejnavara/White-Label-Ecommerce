b937e4546c27d8084c10b5b2ff336c622e03e819
Matej <matej.navara@gmail.com>
2021-01-19 14:41:04 +0000

    Improved spinner component

--- client/src/components/with-spinner/index.jsx
+++ client/src/components/with-spinner/index.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
-import { SpinnerOverlay, SpinnerContainer } from "./styles";
+import Spinner from "../spinner";
 
 const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  ) : (
-    <WrappedComponent {...otherProps} />
-  );
+  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
 };
 
 export default WithSpinner;
