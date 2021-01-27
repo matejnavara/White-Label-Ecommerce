5e3d7c3a4b2f95ebc29a2c15af07c786811d129a
Matej <matej.navara@gmail.com>
2021-01-27 08:28:10 +0000

    Render snapshot for styled comps

--- client/src/components/cart-item/index.test.js
+++ client/src/components/cart-item/index.test.js
@@ -1,9 +1,9 @@
-import { shallow } from "enzyme";
+import { render } from "enzyme";
 import React from "react";
 
 import CartItem from "./index";
 import { mockCartItem } from "../../constants/mockObjects";
 
 it("component renders correctly", () => {
-  expect(shallow(<CartItem item={mockCartItem} />)).toMatchSnapshot();
+  expect(render(<CartItem item={mockCartItem} />)).toMatchSnapshot();
 });
